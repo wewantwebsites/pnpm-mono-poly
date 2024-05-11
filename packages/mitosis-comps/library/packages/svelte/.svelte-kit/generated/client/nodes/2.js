739b759b4d14a3cc44f789a39ecfe0938f362e75
wewantwebsites <cas.guzdziol@wewantweb.site>
2024-05-11 13:59:53 -0400

    so yeah this works but i don't like it

---
+++ packages/mitosis-comps/library/packages/svelte/.svelte-kit/generated/client/nodes/2.js
@@ -0,0 +1,1 @@
+export { default as component } from "../../../../routes/index/+page.svelte";
---
+++ packages/mitosis-comps/test-apps/react/src/App.jsx
@@ -0,0 +1,13 @@
+import { AutoComplete, Todos } from '@mitosis-comps/library-react';
+
+function App() {
+  return (
+    <div>
+      <div>Welcome to React.</div>
+      <AutoComplete />
+      <Todos />
+    </div>
+  );
+}
+
+export default App;
